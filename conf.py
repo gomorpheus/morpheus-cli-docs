9ee85aef034307ae361db8c35ae34d3ce2872113
Jeff Wheeler <jeff@1wheeler.com>
2019-02-26 00:22:35 -0700

    #der

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ source_suffix = ['.rst', '.md']
 # source_suffix = ['.rst', '.md']
 #source_suffix = '.rst'
 
-The master toctree document.
+#The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
