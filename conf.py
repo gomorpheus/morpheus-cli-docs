b8e63e2bf0146029f32685151635508a3918d6dd
Alex Harker <alexnharker@gmail.com>
2023-10-16 18:16:49 -0400

    update configuration once again

--- conf.py
+++ conf.py
@@ -183,4 +183,4 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 def setup(app):
-    app.add_stylesheet('morpheusCliTheme.css')
+    app.add_css_file('morpheusCliTheme.css')
