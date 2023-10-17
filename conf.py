0c28448e058f0d1ee19f3a04fc678b8c3bfdae95
Jeff Wheeler <jeff@1wheeler.com>
2023-10-17 09:42:29 -0600

    try myst_parser in extensions

--- conf.py
+++ conf.py
@@ -30,9 +30,9 @@ rst_prolog = """
 
 #from recommonmark.parser import CommonMarkParser
 
-source_parsers = {
-    '.md': myst_parser,
-}
+#source_parsers = {
+#    '.md': myst_parser,
+#}
 
 source_suffix = ['.rst', '.md']
 
@@ -45,7 +45,9 @@ source_suffix = ['.rst', '.md']
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = []
+extensions = [
+    "myst_parser",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
